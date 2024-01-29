9088823a8bbf6d5e88677840849db15e86fe2633
Irina <149883699+irinakravtsova@users.noreply.github.com>
2024-01-29 06:30:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // создаем переменную, куда будем записывать текущее значение счетчика
-const button = document.querySelector(".btn-add");
-const counterText = document.querySelector(".counter");
-const reset = document.querySelector(".btn-reset");
+const counterEI = document.querySelector('[data-find = "counteValue"]');
+const button = document.querySelector("#addOnePoolButton");
+const counterText = document.querySelector("#addTwoPoolsButton");
+const reset = document.querySelector("#resetButton");
 // переменные вверху - это связь с элементами страницы
 // переменная внизу - данные про реальный счетчик, нажимая на кнопку мы меняем данные счетчика counter = counter + 1; - меняем значение переменной
-let counter = 0;
+const COUNTER_INITIAL_VALUE = 0; //INITIAL - ПРИНЯТОЕ ЗНАЧЕНИЕ ПО УМОЛЧАНИЮ (с которого инициируется переменная)
+// потом присваиваем переменной counter
+let counter = COUNTER_INITIAL_VALUE;
 // начинаем отсчет с 0
 // математические операции сложение/вычитание/умножение/деление и пр.
 // в переменную можно записать математичесое выражение или при выводе в консоль
@@ -25,20 +28,31 @@ let counter = 0;
 
 // обрабатываем нажатие на кнопку
 // создаем переменную button в которую через вызов конструкции document.querySelector (выбыраем элемент в документе/на странице) записали элемент button из html
-console.log(button);
+// console.log(button);
 // добавляем обработчик события(действия), а именно клик. внутри {пишем, что именно хотим сделать}
-button.addEventListener('click', function() {
+addOnePoolButton.addEventListener('click', function() {
 // console.log('произошел клик по кнопке');
 
 // увеличиваем счетчик по клику на кнопку
-counter = counter + 1;
-// console.log(counter);
-counterText.innerText = counter; // выводим в интерфейс с помощью свойства innerText (изменение текста в элементе - виден в интерфейсе) изменяем цифру в счетчике в соответствии с изменениями переменной counter (значение переменной counter подставляется в счетчик counter) 
+counter = counter + parseInt(addOnePoolButton.dataset.pools);
+// data атрибуты строковые, переводим их в число с помощью функции parseInt
+// console.log(counter, addOnePoolButton.dataset.pools);
+
+counterEI.innerText = counter; // выводим в интерфейс с помощью свойства innerText (изменение текста в элементе - виден в интерфейсе) изменяем цифру в счетчике в соответствии с изменениями переменной counter (значение переменной counter подставляется в счетчик counter) 
 });
-reset.addEventListener('click', function() {
-  counter = 0;
-  counterText.innerText = counter;
+addTwoPoolsButton.addEventListener('click', function() {
+    counter = counter + parseInt(addTwoPoolsButton.dataset.pools);
+  counterEI.innerText = counter;
 });
+
+resetButton.addEventListener('click', function() {
+  counter = COUNTER_INITIAL_VALUE;
+counterEI.innerText = counter;
+});
+// reset.addEventListener('click', function() {
+//   counter = 0;
+//   counterText.innerText = counter;
+// });
 
 //DOM - Document Object Model
 
